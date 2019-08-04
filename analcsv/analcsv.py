57c857989bd980c764659bf777fe090d46b38567
Arnoldas Rudzevicius <arnoldas.rudz@gmail.com>
2019-08-05 00:12:04 +0100

    Refactor. Basic outline.

--- analcsv/analcsv.py
+++ analcsv/analcsv.py
@@ -1,105 +1,83 @@
 import csv
-import locale
 import logging
-from string import Template
+import pprint
+from argparse import ArgumentParser
 from collections import Counter, defaultdict
-from argparse import ArgumentParser, FileType
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 
-def display(source):
-    logger.info("{:-^50}".format(" Basic "))
-    for k, v in source.counter.items():
-        if k == "total_rows":
-            logger.info("{}: {:,}".format("Total rows", v))
-        else:
-            logger.info("{}: {}".format(k, v))
-    # logger.info("{:-<50}".format(""))
-
-    logger.info("{:-^50}".format(" Columns "))
-    for column, counter in source.column_counters.items():
-        logger.info("{:_^50}".format(column))
-
-        logger.info(
-            "{:^10} - {:^10} / {:^10} ({:^10})".format(
-                "STAT", "COUNT", "TOTAL ROWS", "DIFFERENCE"
-            )
-        )
-
-        for k, v in counter.items():
-            logger.info(
-                "{:^10} - {:^10,} / {:^10,} ({:^10,})".format(
-                    k, v, source.counter["total_rows"], source.counter["total_rows"] - v
-                )
-            )
+def sniff_dialect(args, line_count=None):
+    lines = ""
+    with open(args.source, encoding=args.file_encoding, newline="") as f:
+        for line_number, line in enumerate(f):
+            if line_count and line_number >= line_count:
+                break
+            else:
+                lines += line
+    return csv.Sniffer().sniff(lines, args.dialect_delimiter)
 
 
-def read(self):
-    for line in self._csv:
-        self.counter["total_rows"] += 1
-        for k, v in line.items():
-            if v:
-                self.column_counters[k]["populated"] += 1
-
-
-def count_total_rows(file, delimiter="|"):
-    c = Counter()
-    with open(file, "r") as f:
-        csv_reader = csv.DictReader(f, delimiter=delimiter)
-        for line in csv_reader:
-            c["ctr"] += 1
-    return c
-
-
-def tester(file, delimiter="|"):
-    c = Counter()
-    with open(file, "r") as f:
-        csv_reader = csv.DictReader(f, delimiter=delimiter)
-        for line in csv_reader:
-            c["tester"] += 1
-    return c
-
-
-def count_column(csv, search_term, target_column=""):
+def command_count(args):
     counter = Counter()
-    column_counters = defaultdict(Counter)
-    for line in csv:
-        line_matched = False
-        for k, v in line.items():
-            if search_term in v:
-                counter["total_field_matches"] += 1
-                column_counters[k]["substring_match"] += 1
-                if search_term == v:
-                    column_counters[k]["exact_match"] += 1
-                if not line_matched:
-                    counter["line_matches"] += 1
-                    line_matched = True
-    return counter, column_counters
+    columns_store = defaultdict(set)
+    if args.column is not None:
+        for column in args.column:
+            columns_store[column] = set()  # For tracking unique values
+    print(columns_store)
+    with open(args.source, encoding=args.file_encoding, newline="") as f:
+        csv_reader = csv.DictReader(f, dialect=args.dialect)
+        for row in csv_reader:
+            counter["total/rows"] += 1
+            for column_name, column_set in columns_store.items():
+                if column_name in row:
+                    column_set.add(row[column_name])
+
+    for column_name, column_set in columns_store.items():
+        counter[f"total/uniques_{column_name}"] = len(column_set)
+
+    return counter
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("source", type=str)
-    parser.add_argument("-d", "--delimiter", type=str, default="|")
-    parser.add_argument("-e", "--encoding", type=str, default="utf-8")
-    args = parser.parse_args()
+    # argparse
+    available_dialects = ",".join(csv.list_dialects())
+    epilog = f"""Available predefined dialects:
+    {', '.join(csv.list_dialects())}.
+    Available special dialect options:
+    sniff (attempts to figure out the dialect from sample of file content;
+    dialect-delimiter option is used),
+    custom (manually builds dialect using dialect-* options)."""
+    parser = ArgumentParser(epilog=epilog)
+    subparsers = parser.add_subparsers(dest="command")
 
-    c = tester(args.source)
-    c2 = count_total_rows(args.source)
-    print(c)
-    print(c2)
-    """
-    with open(args.source, encoding=args.encoding) as f:
-        s = SourceStats(f, delimiter=args.delimiter)
-        c, cc = s.count_column("hi")
-        s.read()
-        print(type(c), type(cc))
-        for i in c.items():
-            print(i)
-        for i in cc.items():
-            print(i)
-        logger.info("{:=^50}".format(" Result "))
-        display(s)
-    """
+    parser.add_argument("source", type=str)
+    parser.add_argument("-v", "--verbose", action="count", default=0)
+    parser.add_argument("--file_encoding", type=str, default="utf-8")
+    parser.add_argument("--dialect", type=str, default="sniff")
+    parser.add_argument("--dialect_delimiter", type=str)
+
+    parser_count = subparsers.add_parser("count")
+    parser_count.add_argument("-c", "--column", action="append", type=str)
+
+    args = parser.parse_args()
+    print(f"{args}")
+
+    if args.dialect == "sniff":
+        args.dialect = sniff_dialect(args, 50)
+    elif args.dialect == "custom":
+        raise NotImplementedError("'custom' as a dialect value.")
+        # TODO: Manually create dialect object from passed dialect-* parameters
+
+    # Logging
+    if args.verbose >= 1:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    # Work
+    if args.command == "count":
+        result = command_count(args)
+        report = pprint.pformat(result)
+        logger.info(f"{report}")
+    else:
+        logger.error("No matching command.")
